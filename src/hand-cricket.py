8ae275bd0c7859d94385231d83d6cc4765cc65c7
abhinavnayak11 <abhinavnayak11@gmail.com>
2022-01-06 11:09:51 +0530

    removed unnecessary comments

--- src/hand-cricket.py
+++ src/hand-cricket.py
@@ -1,4 +1,3 @@
-# branch1
 import cv2
 import numpy as np
 import random
@@ -17,15 +16,7 @@ model = torchvision.models.densenet121(pretrained=True)
 num_ftrs = model.classifier.in_features
 model.classifier = nn.Sequential(
     nn.Linear(num_ftrs, 500),
-
-    # nn.Linear(500,1) : To be used for binary classification and BCEWithLogitsLoss() 
-    # for BCEWithLogitsLoss(), the output from NN should be the logit value for True label
-
-    # nn.Linear(500,2) : To be used for binary classification and CrossEntropyLoss() 
-    # for CrossEntropyLoss(), the output from NN should be the logit values for each class
-
-    nn.Linear(500, 7) # To be used for multi-class classification. 
-    # when using CrossEntropyLoss(), the output from NN should be logit values for each class
+    nn.Linear(500, 7)
 )
 
 # load the saved model
@@ -252,12 +243,12 @@ while True:
         # when different numbers played, add the player's number to score
         if pred.item() != n:
             if player == 'human':
-                if pred.item() == 0:         # add other players number when 0 is played
+                if pred.item() == 0:         # add opposite player's number when 0 is played (that's how we used to play :p)
                     player_scores[player]+=n  
                 else:
                     player_scores[player]+=pred.item()
             else:
-                if n == 0:                   # add other players number when 0 is played
+                if n == 0:                   # add opposite player's number when 0 is played
                     player_scores[player]+=pred.item()
                 else:
                     player_scores[player]+=n
--- src/model.py
+++ src/model.py
@@ -7,9 +7,14 @@ def get_model():
     num_ftrs = model.classifier.in_features
     model.classifier = nn.Sequential(
         nn.Linear(num_ftrs, 500),
+
+        # nn.Linear(500,1) : To be used for binary classification and BCEWithLogitsLoss() 
+        # for BCEWithLogitsLoss(), the output from NN should be the logit value for True label
+
+        # nn.Linear(500,2) : To be used for binary classification and CrossEntropyLoss() 
+        # for CrossEntropyLoss(), the output from NN should be the logit values for each class
+
+        nn.Linear(500, 7) # To be used for multi-class classification. 
         # when using CrossEntropyLoss(), the output from NN should be logit values for each class
-        # nn.Linear(500,1)
-        # when using BCEWithLogitsLoss(), the output from NN should be logit value for True label
-        nn.Linear(500, 7)
     )
     return model
